95ceb7851b71f112175d94473c916f0d9e31c08f
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-03-13 19:43:27 -0500

    Update App.jsx

--- reacttestapp/src/App.jsx
+++ reacttestapp/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { produce, pantryItems } from "./storeItems"
 import ItemList from "./ItemList"
 
 
 function App() {
   const [ cart, setCart ] = useState([])
+  const [ count, setCount ] = useState(0)
 
   const addItem = (item) => {
     setCart((prev) => [item, ...prev]);
@@ -12,12 +13,19 @@ function App() {
   const removeItem = (targetIndex) => {
     setCart((prev) => prev.filter((item, index) => index !== targetIndex))
   }
+  const handleClick = (index) => {
+    removeItem(index);
+
+  }
+  
+
   return (
     <>
       <h1>Grocery</h1>
+      <p>{count}</p>
         <ul>
           {cart.map((item, index) => (
-            <li key={index} onClick={() => removeItem(index)}>{item}</li>
+            <li key={index} onClick={handleClick(index)}>{item}</li>
           ))}
         </ul>
       <h1>Produce</h1>
